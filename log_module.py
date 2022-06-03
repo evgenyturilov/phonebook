d7f2f15ec0a216c32c668a3290b43b40954866af
Evgeny Turilov <67696898+evgenyturilov@users.noreply.github.com>
2022-06-03 10:31:10 +0000

    Added divider and counter to csv logger

--- log_module.py
+++ log_module.py
@@ -5,11 +5,12 @@ def rd_txt():
     print(f'\n{file.read()}')
     
 def logger(data):
-  count = 1 + sum(1 for line in open('phonebook.txt', 'r'))
-  with open('phonebook.txt', 'a') as file:
-    file.write(f'{count}. Имя: {data[0]} {data[1]}, тел. {data[2]}, прим. {data[3]}\n')
-  with open('phonebook.csv', 'a') as file:
+  count = sum(1 for line in open('phonebook.txt', 'r'))
+  with open('phonebook.txt', 'a', encoding='utf8') as file:
+    file.write(f'{count + 1}. Имя: {data[0]} {data[1]}, тел. {data[2]}, прим. {data[3]}\n')
+  with open('phonebook.csv', 'a', encoding='utf8') as file:
+    file.write(f'{count + 1};')
     for i in data:
-      file.write(i)
+      file.write('{};'.format(i))
     file.write('\n')
       
